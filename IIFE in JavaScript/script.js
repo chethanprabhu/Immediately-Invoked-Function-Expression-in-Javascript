3e11116be7f29d36d59e97e3dcad782964de1a39
Chethan Prabhu <chethanprabhu65@gmail.com>
2018-10-28 18:45:11 +0530

    Update script.js

--- IIFE in JavaScript/script.js
+++ IIFE in JavaScript/script.js
@@ -2,3 +2,9 @@
 (function(){
   console.log('Immediately Invoking Function Expression');
 }());
+//The above IIFE can be written using normal function expression like this :
+var func = function(){
+  console.log('some text');
+}
+func();
+//Instead of this we create a anonymous function and invoke it at last using (). Simple.
